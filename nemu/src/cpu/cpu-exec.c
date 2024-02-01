344e5f4605448d371a3204f8106623c9b595c649
tracer-ics2023 <tracer@njuics.org>
2024-02-02 07:37:32 +0800

    >  compile NEMU 3162052051717@glut.edu.cn ChiWeiming Linux chiweiming-VirtualBox 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:37:32 up 12 min,  4 users,  load average: 0.66, 0.23, 0.14

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -99,6 +99,7 @@ void assert_fail_msg() {
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
   g_print_step = (n < MAX_INST_TO_PRINT);
+//	Log("g_print_step = %u", g_print_step, "MAX_INST_TO_PRINT = %d", MAX_INST_TO_PRINT);
   switch (nemu_state.state) {
     case NEMU_END: case NEMU_ABORT:
       printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
