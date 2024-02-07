765566f17d0193fe26b769dfe144c09db0869c21
tracer-ics2023 <tracer@njuics.org>
2024-02-07 09:30:14 +0800

    >  compile NEMU 3162052051717@glut.edu.cn ChiWeiming Linux chiweiming-VirtualBox 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:30:14 up 36 min,  4 users,  load average: 0.07, 0.05, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -71,6 +71,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
 #endif
 }
 
+static void statistic(); 
 void cpu_exec_step() {
 	Decode s;
 	exec_once(&s, cpu.pc);
@@ -78,6 +79,19 @@ void cpu_exec_step() {
 	trace_and_difftest(&s, cpu.pc);
 	if (nemu_state.state != NEMU_RUNNING) return;
 	IFDEF(CONFIG_DEVICE, device_update());
+
+  switch (nemu_state.state) {
+    case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
+
+    case NEMU_END: case NEMU_ABORT:
+      Log("nemu: %s at pc = " FMT_WORD,
+          (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
+           (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
+            ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
+          nemu_state.halt_pc);
+      // fall through
+    case NEMU_QUIT: statistic();
+  }
 }
 
 static void execute(uint64_t n) {
