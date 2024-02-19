e2f7d06271bf36b0caf300554b4cb6435cfe5519
tracer-ics2023 <tracer@njuics.org>
2024-02-19 21:22:22 +0800

    >  compile NEMU 3162052051717@glut.edu.cn ChiWeiming Linux chiweiming-VirtualBox 6.5.0-17-generic #17~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jan 16 14:32:32 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:22 up  9:32,  4 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -62,3 +62,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
+
+void memory_print(unsigned long n, paddr_t addr) {
+
+}
