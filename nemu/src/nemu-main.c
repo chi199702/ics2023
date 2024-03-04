a7e995ff491a99886c219c6f6d14d6de056b7153
tracer-ics2023 <tracer@njuics.org>
2024-03-04 21:13:59 +0800

    >  compile NEMU 3162052051717@glut.edu.cn ChiWeiming Linux chiweiming-VirtualBox 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:13:59 up 1 day, 20:49,  4 users,  load average: 0.02, 0.02, 0.00

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -19,28 +19,34 @@ void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
-
+/*
 int main(int argc, char *argv[]) {
 	static char* dir = "/home/chiweiming/code/ics2023/nemu/tools/gen-expr/build/input";
 	static char buf[65536] = {};
 	FILE* f = fopen(dir, "r");
 	while (fgets(buf, 65535, f)) {
-		printf("%s", buf);
+		char* buf_end = buf + strlen(buf);
+		char* result = strtok(buf, " ");
+		if (buf_end == NULL) { continue; }
+		char* expression = 
 	}
 	
+	return 0;
+}
+*/
+
+int main(int argc, char* argv[]) { 
   /* Initialize the monitor. */
-/*
+
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
 #else
   init_monitor(argc, argv);
 #endif
-*/
+
   /* Start engine. */
-	/*
+	
   engine_start();
 
   return is_exit_status_bad();
-	*/
-	return 0;
 }
